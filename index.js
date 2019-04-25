44954c0538e46094f52c517f3c0f448e374d5536
Felipe Santana <felipeo510.fs@gmail.com>
2019-04-24 21:50:13 -0700

    responsive updates

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
     $('.burger').click(function(){
         $('nav').toggleClass('active')
     });
+
+    $(window).scroll(function(){
+        var scrollFromTop = $(window).scrollTop();
+
+        if(scrollFromTop >= 50){
+           $("header").css("background-color", "rgba(255, 228, 194, 0.88)")
+        } else {
+            $("header").css("background-color", "rgba(130, 83, 51, 0.0)")
+        }
+    });
 });
+
